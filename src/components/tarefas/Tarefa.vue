db42d53b6d9ab6cb3fdb20d6650e3b08c9cd7cde
AllisonMoraes <allisondjkk@gmail.com>
2022-04-04 10:22:31 -0300

    upload do projeto

---
+++ src/components/tarefas/Tarefa.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-list flat subheader>
+      <v-list-item-group
+        multiple
+        active-class=""
+        v-for="(tarefa, index) in tarefas"
+        :key="index"
+      >
+        <v-card>
+          <v-list-item
+            :class="{ 'light-blue lighten-5 ': tarefa.concluido }"
+            @click="tarefa.concluido = !tarefa.concluido"
+          >
+            <template v-slot:default="{}">
+              <v-list-item-action>
+                <v-checkbox
+                  v-model="tarefa.concluido"
+                  @click="tarefa.concluido = !tarefa.concluido"
+                ></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title
+                  :class="{ 'text-decoration-line-through': tarefa.concluido }"
+                  >{{ tarefa.titulo }}</v-list-item-title
+                >
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn 
+                  icon
+                  @click.stop="handleRemoveTarefa(tarefa.id)"
+                >
+                  <v-icon color="red lighten-1">mdi-trash-can</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+        </v-card>
+
+        <v-divider></v-divider>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TarefaC",
+  props: ["tarefas"],
+  methods:{
+    handleRemoveTarefa(id){
+      this.$store.commit('removeTarefa', id)
+      
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-col cols="12">
+      <v-text-field
+        v-model="campoInput"
+        label="Qual a sua tarefa ?"
+        outlined
+        clearable
+        @keyup.enter="handleAddTarefa"
+      ></v-text-field>
+    </v-col>
+
+    <tarefa-c :tarefas="$store.state.tarefas"></tarefa-c>
+  </div>
+</template>
+
+<script>
+import TarefaC from "../components/tarefas/Tarefa.vue";
+
+export default {
+  name: "TarefasView",
+
+  components: {
+    TarefaC,
+  },
+  data() {
+    return {
+      campoInput: null,
+    };
+  },
+  methods: {
+    handleAddTarefa() {
+      this.$store.commit("adicionaTarefa", this.campoInput);
+      this.campoInput = null;
+    },
+  },
+};
+</script>
